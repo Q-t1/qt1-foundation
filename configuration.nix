2058c8cdaae98ed5ed431604c3144ff1289e0a6a
Quentin R. <admin@qt1.fr>
2026-03-15 18:20:31 +0100

    Deployed clusterctl

--- configuration.nix
+++ configuration.nix
@@ -46,7 +46,9 @@
     isNormalUser = true;
     extraGroups = [ "wheel" "libvirtd" "kvm" ];
     packages = with pkgs; [
-      tree
+      jq
+      kubernetes-helm
+      clusterctl
     ];
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDtASdfLMatnUWsdJIjIvIXqXrnmABAznN/6mji1/rzRLqrusduqahyi4htTRvOuue3vrhUqeywiRTNTpzthfhVqeF5WehE1wAPkbgGwAvxC8ltqLPza6KkfZF0WXdXj/MsKJDTJUwui+acbyJocuMz0teJOhURoaEetXzr+ffj6P9Txz7uX6KN8D2DYGi9WvG8QPdlF/89f5vtCx4GFrKkdSET+yNC3PEcf+X8wDoL+ztuvcTGLb4rC42NzLJ82VCAYZ6KS085s8GD+lcgU/jxpRUeCVoY7Ciym/VKs2oxVsyM45fP+d33BJmqV+WGgVLHz0T4y05HOS6CBLObbXZYLfDg7jNl/MVxVktNRfvPLr23z8IvUL1DR8lHIqc6jesFMe8W5PuaoxwzQIhRl8ywGT/rVq1btMiS41mqo/86pZAFtehTt04A3GbMVGB7NNO3tmaVbUlr/aSFdB/hLr0pU3uuZQsHCipZ/3+IGs7erU1r2VVNhnxd/JcDJEVstd8= quentin@MacBook-Air-de-Quentin.local"
@@ -72,9 +74,7 @@
     vim
     iputils
     ghostty
-    openssl
     k3s
-    kubernetes-helm
     ];
 
   boot.kernelModules = [ "kvm-intel" "kvm-amd" ];
@@ -88,11 +88,15 @@
     role = "server";
     extraFlags = toString [
       "--disable=traefik"
-      "--write-kubeconfig-group wheel"
+      "--write-kubeconfig-mode 640"
+      "--write-kubeconfig-group k3sconfig"
     ];
   };
 
-  environment.variables.KUBECONFIG = "/etc/rancher/k3s/k3s.yaml";
+  # required for rootless access
+  environment.variables = {
+    KUBECONFIG = "/etc/rancher/k3s/k3s.yaml";
+  };
 
   services = {
     openssh = {
